11dca6fab1192b124c2b0cc57540cf05fbc2ab00
lzx <a1928564318@outlook.com>
2024-08-23 15:22:32 +0800

    更新笔记

--- java/源码/Redis/Redis-Jedis-Lettuce-RedisTemplate-Demo/src/main/java/com/springboot/example/redis/config/RedisConfig.java
+++ java/源码/Redis/Redis-Jedis-Lettuce-RedisTemplate-Demo/src/main/java/com/springboot/example/redis/config/RedisConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
